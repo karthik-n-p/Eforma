666615eafeefefa5fa81e325a2a67c1dd25e8ff5
karthik-n-p <knp5826@gmail.com>
2025-02-28 21:47:27 +0530

    +minor refinement

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import { motion } from "framer-motion";
 import { File, PlayCircle, ScrollText, MessageCircle, Play } from "lucide-react";
 import { Link } from "react-router-dom";
